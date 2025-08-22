158c9a6f919f2bd51074a99754055e147576b0bd
ftMadrid <fernandomadridcv04@gmail.com>
2025-08-22 08:54:30 -0600

    clase RentItem terminada

---
+++ Lab2Examen1/src/lab2examen1/RentItem.java
@@ -0,0 +1,39 @@
+package lab2examen1;
+
+import javax.swing.ImageIcon;
+
+public abstract class RentItem {
+    
+    protected int codigo;
+    protected String nombre;
+    protected double precio_renta;
+    protected int cantidadCopias = 0;
+    protected ImageIcon imagen;
+    
+    public RentItem(int codigo, String nombre, double precio_renta) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio_renta = precio_renta;
+    }
+    
+    public String toString() {
+        return "\n"
+                +"\n| Codigo: "+codigo
+                +"\n| Nombre: "+nombre
+                +String.format("\n| Precio de Renta: Lps.%.2f", precio_renta);
+    }
+    
+    public abstract double pagoRenta(int dias);
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio_renta() {
+        return precio_renta;
+    }
+}
